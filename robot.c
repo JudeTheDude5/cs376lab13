cae88567ee00ad1af22f62e06f93c2888c18d561
reynoldj26 <reynoldj26@engineering07.campus.up.edu>
2023-11-30 12:08:44 -0800

    Gave the robot a       facelift, added myself to authro list

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Jude
  * @since August 2013
  *
  */
@@ -36,9 +36,9 @@ void robotPrintAscii(void)
   printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
 	 "%s    i_i    \n"
-	 "%s   [@_@]   \n"
+	 "%s   [*^*]   \n"
 	 "%s  /|___|\\ \n"
-	 "%s   d   b   \n",blank, blank, blank, blank);
+	 "%s   |   |   \n",blank, blank, blank, blank);
 
   blank = blank - 10;
 }
@@ -55,15 +55,15 @@ void dalekPrintAscii(void)
   printf("\n\n"
 	 "I am Dalek, from Dr. Who\n"
 	 "%s      <----->      \n" 
-         "%s     <  (0)  >     \n"        
-         "%s     |       |     \n"
+         "%s     <  ($)  >     \n"        
+         "%s     |   O   |     \n"
          "%s    < ------- >    \n"
          "%s    o         o    \n"
          "%s    o  0  ()  o    \n"
 	 "%s   o           o   \n"
    	 "%s o o o o o o o o o \n"
    	 "%s o o o o o o o o o \n"
-   	 "%s o o o o o o o o o \n", blank, blank, blank, blank, blank, blank, 
+   	 "%s o o o o o o o o 0 \n", blank, blank, blank, blank, blank, blank, 
 	 blank, blank, blank, blank);
   blank = blank - 10;
 }
